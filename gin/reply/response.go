92b3716478275f8b5393443e2eceb4579b9d3e50
solate <349370626@qq.com>
2019-05-27 19:51:03 +0800

    merge to response func  统一成一个返回值方法

--- gin/reply/response.go
+++ gin/reply/response.go
@@ -6,8 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 服务回复信息
-type Response struct {
+// 服务回复结构
+type response struct {
+	HttpCode int `json:"http_code"` // 响应服务http状态码
+	body
+}
+
+// 回复body结构
+type body struct {
 	ErrCode int         `json:"err_code"` // 错误码
 	ErrMsg  string      `json:"err_msg"`  // 错误提示信息
 	Data    interface{} `json:"data"`     // 返回数据
@@ -18,48 +24,47 @@ const (
 	ErrSuccessMsg  = "success"
 )
 
-// 返回成功
-func Success(c *gin.Context, data interface{}) {
-	c.JSON(http.StatusOK, &Response{ErrSuccessCode, ErrSuccessMsg, data})
-}
-
-// 返回失败, 但是参数可以用方法设置
-//options: httpCode, errCode int, errMsg string, data
-func Error(c *gin.Context, httpCode int, options ...func(*Response)) {
-
+func Response(c *gin.Context, options ...func(*response)) {
 	// 设置参数
-	res := &Response{}
+	res := &response{http.StatusOK, body{ErrCode: ErrSuccessCode, ErrMsg: ErrSuccessMsg}}
 	for _, option := range options {
 		option(res)
 	}
-	c.JSON(httpCode, res)
+	c.JSON(res.HttpCode, res.body)
+}
+
+// 设置数据
+func SetHttpCode(code int) func(*response) {
+	return func(response *response) {
+		response.HttpCode = code
+	}
 }
 
 // 设置错误码, 一般有code 就会有msg
-func SetErrCodeAndMsg(code int, msg string) func(*Response) {
-	return func(response *Response) {
+func SetErrCodeAndMsg(code int, msg string) func(*response) {
+	return func(response *response) {
 		response.ErrCode = code
 		response.ErrMsg = msg
 	}
 }
 
 // 设置数据
-func SetData(data interface{}) func(*Response) {
-	return func(response *Response) {
+func SetData(data interface{}) func(*response) {
+	return func(response *response) {
 		response.Data = data
 	}
 }
 
 // 设置错误码
-func SetErrCode(code int) func(*Response) {
-	return func(response *Response) {
+func SetErrCode(code int) func(*response) {
+	return func(response *response) {
 		response.ErrCode = code
 	}
 }
 
 // 设置错误码
-func SetErrMsg(msg string) func(*Response) {
-	return func(response *Response) {
+func SetErrMsg(msg string) func(*response) {
+	return func(response *response) {
 		response.ErrMsg = msg
 	}
 }
--- gin/reply/response_test.go
+++ gin/reply/response_test.go
@@ -6,46 +6,65 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSuccess(t *testing.T) {
+func TestResponse(t *testing.T) {
+
+	// success 成功
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
-
-	Success(c, gin.H{"foo": "bar"})
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "{\"err_code\":200000,\"err_msg\":\"success\",\"data\":{\"foo\":\"bar\"}}", w.Body.String())
+	Response(c)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"err_code\":200000,\"err_msg\":\"success\",\"data\":null}", w.Body.String())
 }
 
-func TestError(t *testing.T) {
+// 测试设置http状态码
+func TestSetHttpCode(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	Response(c, SetHttpCode(http.StatusForbidden))
+
+	assert.Equal(t, http.StatusForbidden, w.Code)
+	assert.Equal(t, "{\"err_code\":200000,\"err_msg\":\"success\",\"data\":null}", w.Body.String())
+}
+
+// 测试设置错误码
+func TestSetErrCode(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	Response(c, SetErrCode(400403))
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"err_code\":400403,\"err_msg\":\"success\",\"data\":null}", w.Body.String())
+
+}
+
+func TestSetErrMsg(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	Response(c, SetErrMsg("params error"))
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"err_code\":200000,\"err_msg\":\"params error\",\"data\":null}", w.Body.String())
+}
+
+func TestSetErrCodeAndMsg(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	Response(c, SetErrCode(400403), SetErrMsg("params error"))
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"err_code\":400403,\"err_msg\":\"params error\",\"data\":null}", w.Body.String())
+}
+
+func TestSetData(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
+	Response(c, SetData(gin.H{"foo": "bar"}))
 
-	Error(c, http.StatusForbidden, SetErrCodeAndMsg(400403, "params error"))
-
-	assert.Equal(t, http.StatusForbidden, w.Code)
-	assert.Equal(t, "{\"err_code\":400403,\"err_msg\":\"params error\",\"data\":null}", w.Body.String())
-
-	// 加 code, msg 单独
-	w = httptest.NewRecorder()
-	c, _ = gin.CreateTestContext(w)
-
-	Error(c, http.StatusForbidden, SetErrCode(400403), SetErrMsg("params error"), SetData(gin.H{"foo": "bar"}))
-
-	assert.Equal(t, http.StatusForbidden, w.Code)
-	assert.Equal(t, "{\"err_code\":400403,\"err_msg\":\"params error\",\"data\":{\"foo\":\"bar\"}}", w.Body.String())
-
-	// 加 data
-	w = httptest.NewRecorder()
-	c, _ = gin.CreateTestContext(w)
-
-	Error(c, http.StatusForbidden, SetErrCodeAndMsg(400403, "params error"), SetData(gin.H{"foo": "bar"}))
-
-	assert.Equal(t, http.StatusForbidden, w.Code)
-	assert.Equal(t, "{\"err_code\":400403,\"err_msg\":\"params error\",\"data\":{\"foo\":\"bar\"}}", w.Body.String())
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"err_code\":200000,\"err_msg\":\"success\",\"data\":{\"foo\":\"bar\"}}", w.Body.String())
 
 }
